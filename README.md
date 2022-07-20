b390f36e6f00b2658209daa558be525a2b04c864
penghaihan <846040793@qq.com>
2022-07-20 09:58:52 +0800

    third commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 利用pocsuite3框架编写的poc
 
+# pocsuit3 POC脚本
+
--- rce_佑友防火墙/rce_佑友防火墙.py
+++ rce_佑友防火墙/rce_佑友防火墙.py
@@ -13,21 +13,21 @@ class H3c_Iem(POCBase):
     # fofa语句: title="vRealize Operations Manager"
     vulID = "0"  # ssvid ID 如果是提交漏洞的同时提交 PoC,则写成 0
     version = "1"  # 默认为1
-    author = "知了"  # PoC作者的大名
+    author = "Pontusec"  # PoC作者的大名
     vulDate = "2021-11-11"  # 漏洞公开的时间,不知道就写今天
     createDate = "2021-11-11"  # 编写 PoC 的日期
     updateDate = "2021-11-11"  # PoC 更新的时间,默认和编写时间一样
     references = [""]  # 漏洞地址来源,0day不用写
-    name = "H3C IMC dynamiccontent.properties.xhtm 远程命令执行 PoC"  # PoC 名称
+    name = "远程命令执行 PoC"  # PoC 名称
     appPowerLink = ""  # 漏洞厂商主页地址
-    appName = "H3C IMC "  # 漏洞应用名称
+    appName = "rce "  # 漏洞应用名称
     appVersion = "all"  # 漏洞影响版本
     vulType = VUL_TYPE.COMMAND_EXECUTION # XML实体注入 漏洞类型,类型参考见 漏洞类型规范表
     category = POC_CATEGORY.EXPLOITS.WEBAPP  # poc对应的产品类型 web的
     # samples = []  # 测试样列,就是用 PoC 测试成功的网站
     # install_requires = []  # PoC 第三方模块依赖，请尽量不要使用第三方模块，必要时请参考《PoC第三方模块依赖说明》填写
-    desc = """H3C IMC dynamiccontent.properties.xhtm 远程命令执行"""  # 漏洞简要描述
-    pocDesc = """pocsuite -r pocs/h3c_iem.py -u http://example --verify"""  # POC用法描述
+    desc = """youyou"""  # 漏洞简要描述
+    pocDesc = """pocsuite -r pocs/xx.py -u http://example --verify"""  # POC用法描述
 
     def _check(self):
         result = []
